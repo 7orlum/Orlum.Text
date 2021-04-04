c2fed6e048b17d6c3d54b6d62843d59836c83431
7orlum <7orlum@gmail.com>
2021-04-04 15:12:11 +0200

    Add parameters checking for null

--- src/Text/Format.cs
+++ src/Text/Format.cs
@@ -5,7 +5,7 @@ using System.Globalization;
 
 namespace Orlum.Text
 {
-    public class Format
+    public static class Format
     {
         private static readonly Dictionary<CultureInfo, NumericalPhraseFormatProvider> _numericalPhraseFormatProviders = new();
         private static readonly Dictionary<CurrencyValueFormatProviderKey, CurrencyValueFormatProvider> _currencyValueFormatProviders = new();
@@ -13,6 +13,9 @@ namespace Orlum.Text
 
         public static string NP(FormattableString formattableString, CultureInfo cultureInfo = null)
         {
+            if (formattableString == null)
+                throw new ArgumentNullException(nameof(formattableString));
+
             if (!_numericalPhraseFormatProviders.ContainsKey(cultureInfo ?? CultureInfo.CurrentCulture))
                 _numericalPhraseFormatProviders.Add(cultureInfo ?? CultureInfo.CurrentCulture, new NumericalPhraseFormatProvider(cultureInfo));
 
@@ -28,6 +31,12 @@ namespace Orlum.Text
 
         public static string CV(FormattableString formattableString, string isoCurrencySymbol, bool useCurrencySpecificPositiveNegativePatterns, CultureInfo cultureInfo = null)
         {
+            if (formattableString == null)
+                throw new ArgumentNullException(nameof(formattableString));
+
+            if (isoCurrencySymbol == null)
+                throw new ArgumentNullException(nameof(isoCurrencySymbol));
+
             var key = new CurrencyValueFormatProviderKey(isoCurrencySymbol, useCurrencySpecificPositiveNegativePatterns, cultureInfo);
 
             if (!_currencyValueFormatProviders.ContainsKey(key))
